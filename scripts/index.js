c1c6164a300ed88f8b6a288247cc630b193f22b7
heeecker <89743412+filip326@users.noreply.github.com>
2023-02-23 14:07:40 -0800

    cleaned up code, small improvements

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,48 @@ document.querySelector('#nav_expand').addEventListener('change', (e) => {
     }
 })
 
-document.querySelector('header h2').addEventListener('click', () => {
+const nameTag = document.querySelector('header h2')
+
+nameTag.addEventListener('click', () => {
     window.location.href = "index.html"
 })
 
-document.querySelector('header h2').addEventListener('mouseover', () => {
+nameTag.addEventListener('mouseover', () => {
     document.querySelector('header h2').innerText = "der Bahnbabo";
+    if (!playing) {
+        toggleSong()
+        playingBecauseOfHover = true;
+    }
 })
 
-document.querySelector('header h2').addEventListener('mouseout', () => {
+nameTag.addEventListener('mouseout', () => {
     document.querySelector('header h2').innerText = "Peter Wirth";
+    if (playing && playingBecauseOfHover) {
+        toggleSong()
+        playingBecauseOfHover = false;
+    }
 })
+
+let playing = false;
+let playingBecauseOfHover = false;
+function toggleSong() {
+    if (!playing) {
+        document.querySelector('audio').play()
+        document.querySelector('#title_img').style.outlineColor = "#34b1eb";
+        document.querySelector('#title_img').style.outlineWidth = "10px";
+        document.querySelector('#title_img').style.outlineOffset = "10px";
+        document.querySelector('#title_img').style.outlineStyle = "dashed";
+        document.querySelector('#title_audio').innerText = "anhalten"
+        playing = true
+        console.debug('playing')
+    } else {
+        document.querySelector('audio').pause()
+        document.querySelector('#title_img').style.outlineWidth = "4px";
+        document.querySelector('#title_img').style.outlineColor = "#fff";
+        document.querySelector('#title_img').style.outlineStyle = "solid";
+        document.querySelector('#title_img').style.outlineOffset = "0px";
+        document.querySelector('#title_audio').innerText = "abspielen"
+        playing = false
+        console.debug('stoped')
+    }
+}
